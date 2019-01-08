49ed900a2747023c1bd92d4116cccbeb9654b4bb
Michael Wheeler <michaelwheeler@Michaels-MacBook-Pro.local>
2019-01-08 11:11:28 +0000

    Updated vendor interface with getCommutes method

--- src/main/java/com/cycle_saver/controller/ActivityVendor/ActivityVendor.java
+++ src/main/java/com/cycle_saver/controller/ActivityVendor/ActivityVendor.java
@@ -3,10 +3,8 @@ package com.cycle_saver.controller.ActivityVendor;
 import com.cycle_saver.model.Activity;
 import com.cycle_saver.model.User;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ActivityVendor {
-    public List<Activity> extractActivities(User user) throws IOException;
+    List<Activity> getCommutes(User user);
 }
