1f4b5ee9b7e64b1c1a4cd6ed67b659696fec9fa8
Michael Wheeler <michaelwheeler@Michaels-MacBook-Pro.local>
2019-01-21 14:40:33 +0000

    Renamed TFLClient to TFLConnector Cleaned REST Requests Removed UserControllers for now Cleaned StravaController TFL Logic

--- src/main/java/com/cycle_saver/connectors/StravaConnector.java
+++ src/main/java/com/cycle_saver/connectors/StravaConnector.java
@@ -1,14 +1,10 @@
 package com.cycle_saver.connectors;
 
 import com.cycle_saver.model.Strava.Activity;
-import com.cycle_saver.model.Strava.Athlete;
 import com.cycle_saver.model.Strava.StravaToken;
-import com.cycle_saver.model.user.User;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -18,11 +14,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -36,56 +29,39 @@ public class StravaConnector {
         this.httpclient = HttpClients.createDefault();
     }
 
-    public List<Activity> extractActivities(User user) {
-
-        try {
-            URI uri = new URIBuilder()
-                    .setScheme("https")
-                    .setHost("www.strava.com")
-                    .setPath("/api/v3/athletes/" + user.getAthlete().getId() + "/activities")
-                    .setParameter("access_token", user.getStravaAthleteToken())
-                    .setParameter("per_page", "200")
-                    .setParameter("nationalSearch", "true")
-                    .build();
-            HttpGet httpget = new HttpGet(uri);
-            HttpResponse response = httpclient.execute(httpget);
-            String output = EntityUtils.toString(response.getEntity());
-            return parseActivitiesResponse(output);
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-
-    public List<Activity> parseActivitiesResponse(String response) {
-        return new Gson().fromJson(response, new TypeToken<ArrayList<Activity>>() {
-        }.getType());
-    }
-
     public StravaToken requestAccessToken(String authCode) throws IOException {
         HttpPost httppost = new HttpPost("https://www.strava.com/oauth/token");
-
         List<NameValuePair> params = new ArrayList<NameValuePair>(3);
+        //TODO Remove Strava secrets
         params.add(new BasicNameValuePair("client_id", "28817"));
         params.add(new BasicNameValuePair("client_secret", "f0949137a6f81f4748fedfe204b28c7d02bfb46e"));
         params.add(new BasicNameValuePair("code", authCode));
         httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-
         HttpResponse response = httpclient.execute(httppost);
-        HttpEntity entity = response.getEntity();
-        InputStream instream = entity.getContent();
-        String accessTokenResponse = IOUtils.toString(instream, "UTF-8");
-        System.out.println("ACCESS TOKEN RESPONSE: " + accessTokenResponse);
-        StravaToken token = parseAccessTokenResponse(accessTokenResponse);
-        Athlete athlete = token.getAthlete();
-        // store athlete info and add to user data
-        // store token for later use
-        StravaToken stravaToken = parseAccessTokenResponse(accessTokenResponse);
-        return stravaToken;
+        return parseAccessTokenResponse(IOUtils.toString(response.getEntity().getContent(), "UTF-8"));
     }
 
     public StravaToken parseAccessTokenResponse(String accessTokenResponse) {
         return new Gson().fromJson(accessTokenResponse, StravaToken.class);
+    }
+
+    public List<Activity> getActivities(int athleteId, String accessToken) throws URISyntaxException, IOException {
+        URI uri = new URIBuilder()
+                .setScheme("https")
+                .setHost("www.strava.com")
+                .setPath("/api/v3/athletes/" + athleteId + "/activities")
+                .setParameter("access_token", accessToken)
+                .setParameter("per_page", "200")
+                .setParameter("nationalSearch", "true")
+                .build();
+        HttpGet httpget = new HttpGet(uri);
+
+        HttpResponse response = httpclient.execute(httpget);
+        return parseActivitiesResponse(IOUtils.toString(response.getEntity().getContent(), "UTF-8"));
+    }
+
+    public List<Activity> parseActivitiesResponse(String response) {
+        return new Gson().fromJson(response, new TypeToken<ArrayList<Activity>>() {
+        }.getType());
     }
 }
--- src/main/java/com/cycle_saver/controller/strava/StravaController.java
+++ src/main/java/com/cycle_saver/controller/strava/StravaController.java
@@ -1,6 +1,7 @@
 package com.cycle_saver.controller.strava;
 
 import com.cycle_saver.connectors.StravaConnector;
+import com.cycle_saver.connectors.TFLConnector;
 import com.cycle_saver.controller.BaseController;
 import com.cycle_saver.model.Strava.Activity;
 import com.cycle_saver.model.Strava.Athlete;
@@ -8,25 +9,19 @@ import com.cycle_saver.model.Strava.StravaAuth;
 import com.cycle_saver.model.Strava.StravaToken;
 import com.cycle_saver.model.user.User;
 import com.google.gson.Gson;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.impl.client.HttpClients;
-import com.google.gson.reflect.TypeToken;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
@@ -40,7 +35,7 @@ public class StravaController extends BaseController {
     @RequestMapping(value = "/connect", method = RequestMethod.POST)
     @Produces(MediaType.APPLICATION_JSON)
     public Response connect(@RequestParam(value = "user") Integer user) throws IOException {
-        //localhost:5000%2Fauth%2Fstrava
+        //localhost:5000/auth/strava
         String url = "https://www.strava.com/oauth/authorize?client_id=28817&response_type=code" +
                 "&redirect_uri=http://localhost:5000%2Fstrava%2Fauth&approval_prompt=force";
 
@@ -55,8 +50,8 @@ public class StravaController extends BaseController {
     @RequestMapping(value = "/auth", method = RequestMethod.GET)
     @Produces(MediaType.APPLICATION_JSON)
     public Response auth(@RequestParam(value = "state") String state,
-                  @RequestParam(value = "code") String code,
-                  @RequestParam(value = "scope") String scope) throws IOException {
+                         @RequestParam(value = "code") String code,
+                         @RequestParam(value = "scope") String scope) throws IOException {
 
         try {
             User u = null;
@@ -72,65 +67,33 @@ public class StravaController extends BaseController {
             logger.info("Authorisation Information is: " + stravaAuth.toString());
             StravaToken token = stravaConnector.requestAccessToken(code);
             Athlete athlete = token.getAthlete();
+
             // update our record of user with strava info
             u.setAthlete(athlete);
-            // return athlete info to display on FE
 
-            getRoutes(athlete, "e2192a3cebe2872cc31c4df1b3515b1ad4148abf");
-            System.out.println("FILTERING COMMUTES");
+            // update user with journeys
+            List<Activity> activities = stravaConnector.getActivities(athlete.getId(), token.getAccessToken());
             filterCommutes(athlete);
+            TFLConnector tflController = new TFLConnector();
+            for (Activity activity : activities) {
+                try {
+                    u.addJourney(tflController.buildJourney(activity));
+                } catch (IOException | URISyntaxException e) {
+                    e.printStackTrace();
+                }
+            }
 
-            // update / insert updated/new user
-
-//        TFLController tfl = new TFLController();
-//        athlete.getActivities().forEach(activity -> user.addJourney(tfl.calculateJourney(
-//                activity,
-//                "421beaf3651ef6dcea93a05e0bb3dd86",
-//                "621b4307")));
-
-
+            // return athlete info to display on FE
             return Response.status(302).location(new URI(webUrl)).build();
         } catch (URISyntaxException u) {
             logger.severe("Failed to set uri: " + u.getMessage());
         }
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-
-    }
-
-    @RequestMapping(value = "/routes", method = RequestMethod.GET)
-    @Produces(MediaType.APPLICATION_JSON)
-    public void getRoutes(Athlete athlete, String accessToken) throws IOException {
-        HttpClient httpclient = HttpClients.createDefault();
-        URI uri = null;
-        try {
-            uri = new URIBuilder()
-                    .setScheme("https")
-                    .setHost("www.strava.com")
-                    .setPath("/api/v3/athletes/" + athlete.getId() + "/activities")
-                    .setParameter("access_token", accessToken)
-                    .build();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        HttpGet httpget = new HttpGet(uri);
-        System.out.println(httpget.getURI());
-
-        HttpResponse response = httpclient.execute(httpget);
-        HttpEntity entity = response.getEntity();
-        InputStream instream = entity.getContent();
-        String output = IOUtils.toString(instream, "UTF-8");
-        parseActivitiesResponse(athlete, output);
-    }
-
-    private void parseActivitiesResponse(Athlete athlete, String results) throws IOException {
-        ArrayList<Activity> activities  = new Gson().fromJson(results, new TypeToken<ArrayList<Activity>>(){}.getType());;
-        athlete.setActivities(activities);
-        System.out.println(athlete.getActivities().toString());
-        athlete.getActivities().forEach(activity -> System.out.println("Is Commute? " + activity.getCommute() + "   Activity Name: " + activity.getName() + "\n"));
     }
 
     private void filterCommutes(Athlete athlete) {
         athlete.getActivities().removeIf(activity -> !activity.getCommute());
+        //TODO Remove
         athlete.getActivities().forEach(activity -> System.out.println("Is Commute? " + activity.getCommute() + "   Activity Name: " + activity.getName() + "\n"));
     }
 }
